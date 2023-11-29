c3ab4da2c14a0d6e37b2a4bb55e2b860dbf00551
Bryce Callender <brycec848@gmail.com>
2023-11-28 18:25:14 -0800

    orange?

--- costar.py
+++ costar.py
@@ -4,7 +4,7 @@ import random
 
 tree = RGBXmasTree()
 
-costar_colors = [Color('red'), Color('blue'), Color('green'), Color.from_rgb(1.0, 0.34, 0.2)]
+costar_colors = [Color('red'), Color('blue'), Color('green'), Color.from_hsv(11, 0.80, 0.60)]
 
 try:
     while True:
